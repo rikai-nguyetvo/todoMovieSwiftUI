a7a565d1e5e4178cde56594988e9882068ff5e6b
aVuDang <dang.nguyenvu@access-company.com>
2023-03-21 13:58:17 +0700

    Initial Commit

---
+++ ToDoMovies/ToDoMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoMoviesApp.swift
+//  ToDoMovies
+//
+//  Created by RTS Mini on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
